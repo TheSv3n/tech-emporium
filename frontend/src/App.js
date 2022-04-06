704ac5e0418fdfc5abc69eb4659786d1fb348020
Steven Creighton <ste.creighton@googlemail.com>
2022-04-06 17:03:24 +0100

    06/04/2022

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
